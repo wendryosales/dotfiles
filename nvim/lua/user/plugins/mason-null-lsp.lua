92c8d05f6711726efb373d5796191f8c9ef8fa4e
wendryo <wendryo.sales@gmail.com>
2023-11-16 10:33:31 -0300

    feat: add config for prettier and eslint

---
+++ nvim/lua/user/plugins/mason-null-lsp.lua
@@ -0,0 +1,27 @@
+return {
+  "jay-babu/mason-null-ls.nvim",
+  opts = {
+    handlers = {
+      -- for prettier
+      prettier = function()
+        require("null-ls").register(require("null-ls").builtins.formatting.prettier.with {
+          condition = function(utils)
+            return utils.root_has_file "package.json"
+                or utils.root_has_file ".prettierrc"
+                or utils.root_has_file ".prettierrc.json"
+                or utils.root_has_file ".prettierrc.js"
+          end,
+        })
+      end,
+      eslint_d = function()
+        require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+          condition = function(utils)
+            return utils.root_has_file "package.json"
+                or utils.root_has_file ".eslintrc.json"
+                or utils.root_has_file ".eslintrc.js"
+          end,
+        })
+      end,
+    },
+  },
+}
